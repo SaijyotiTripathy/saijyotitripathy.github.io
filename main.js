07839ffce89255e785345ad86e5c8f802b691a37
Saijyoti Tripathy <81572122+SaijyotiTripathy@users.noreply.github.com>
2021-07-15 18:42:43 +0530

    Update main.js

--- main.js
+++ main.js
@@ -25,6 +25,19 @@ function linkAction(){
 }
 navLink.forEach(n => n.addEventListener('click', linkAction));
 
+// Dark Mode
+let darkmode = document.querySelector('#darkmode');
+
+darkmode.onclick = () => {
+    if(darkmode.classList.contains('bx-moon')){
+        darkmode.classList.replace('bx-moon','bx-sun');
+        document.body.classList.add('active');
+    }else{
+        darkmode.classList.replace('bx-sun','bx-moon');
+        document.body.classList.remove('active');
+    }
+}
+
 /*===== SCROLL REVEAL ANIMATION =====*/
 const sr = ScrollReveal({
     origin: 'top',
